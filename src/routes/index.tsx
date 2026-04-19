cbd1281eb4779d89f050b943abf1fdc56cc7e599
SergioLNeves <sergioluiz852@gmail.com>
2026-04-19 18:36:08 -0300

    feat: add sync with toolkit agents

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,7 @@ export const Route = createFileRoute("/")({
 function Home() {
   return (
     <div className="flex flex-col container justify-center items-center min-w-screen max-w-md px-4 py-10">
-      <div className="flex flex-col max-w-8xl gap-16 ">
+      <div className="flex flex-col max-w-8xl gap-16">
         <Hero />
         <Install />
         <Metrics {...metricsMock} />
